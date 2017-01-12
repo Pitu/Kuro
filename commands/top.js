c69c40d9b4dd9a684e01d2890610b6cb58b0a714
RyoshiKayo <caymon@ryoshicayo.net>
2017-01-11 19:47:44 -0800

    added =

--- commands/top.js
+++ commands/top.js
@@ -33,7 +33,7 @@ Number.prototype.toFixedDown = function(digits) {
     return m ? parseFloat(m[1]) : this.valueOf();
 };
 sizeOf = function (bytes) {
-  if (bytes == 0) { return "0.00 B"; }
+  if (bytes === 0) { return "0.00 B"; }
   var e = Math.floor(Math.log(bytes) / Math.log(1024));
   return (bytes/Math.pow(1024, e)).toFixed(2)+' '+' KMGTP'.charAt(e)+'B';
 }
