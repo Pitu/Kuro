46952e329deac9042449cb35a092f1f50763fd50
RyoshiKayo <caymon@ryoshicayo.net>
2017-01-15 12:13:52 -0800

    Update lastfm.js
    Added check for user api and username

--- commands/lastfm.js
+++ commands/lastfm.js
@@ -2,15 +2,17 @@ let kuro
 exports.init = function(bot){ kuro = bot }
 
 function updatePlaying() {
-    let statement = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${kuro.config.lastfm.username}&api_key=${kuro.config.lastfm.apiKey}&format=json&nowplaying=true`
-    const child = require('child_process');
-    child.exec(`curl -s "${statement}"`, (error, stdout, stderr) => {
-        let name = JSON.parse(stdout).recenttracks.track[0].name;
-        let artist = JSON.parse(stdout).recenttracks.track[0].artist['#text'];
-        kuro.modules['playing'].save(`${name} by ${artist}`)
-        kuro.user.setGame(`${name} by ${artist}`)
-        kuro.log(`Now listening to: ${name} by ${artist}`)
-    });
+    if (kuro.config.lastfm.apiKey || kuro.config.lastfm.username === '') {
+        let statement = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${kuro.config.lastfm.username}&api_key=${kuro.config.lastfm.apiKey}&format=json&nowplaying=true`
+        const child = require('child_process');
+        child.exec(`curl -s "${statement}"`, (error, stdout, stderr) => {
+            let name = JSON.parse(stdout).recenttracks.track[0].name;
+            let artist = JSON.parse(stdout).recenttracks.track[0].artist['#text'];
+            kuro.modules['playing'].save(`${name} by ${artist}`)
+            kuro.user.setGame(`${name} by ${artist}`)
+            kuro.log(`Now listening to: ${name} by ${artist}`)
+        });
+    }
 }
 
 exports.run = function(msg, args) {
