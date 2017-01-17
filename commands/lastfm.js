ee5948ee06e3b0515b06a42c811fa05a3f63f7b4
RyoshiKayo <caymon@ryoshicayo.net>
2017-01-17 11:13:51 -0800

    REQUEST update
    Removed child process and curl, implemented REQUEST module

--- commands/lastfm.js
+++ commands/lastfm.js
@@ -1,15 +1,16 @@
 let kuro
-let config = require('.././files/lastfm.js')
+let _config = require('.././files/lastfm.js')
+let request = require('request');
 const currentSong = {'name': '', 'artist': ''};
 exports.init = function(bot) {
-        if (config.enabled === true)
+        if (_config.enabled === true)
         {
-                if (config.username != '' || config.username != null)
+                if (_config.username != '' || _config.username != null)
                 {
-                        if (config.apiKey != '' || config.apiKey != null)
+                        if (_config.apiKey != '' || _config.apiKey != null)
                         {
                                 kuro.log('Last.FM enabled and running!')
-                                setInterval(updatePlaying, config.refresh);
+                                setInterval(updatePlaying, _config.refresh);
                         } else {
                                 kuro.log('Invalid Last.FM API key!')
                         }
@@ -21,19 +22,37 @@ exports.init = function(bot) {
         }
 }
 
-function updatePlaying() {
-    let statement = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${config.username}&api_key=${config.apiKey}&format=json&nowplaying=true`
-    const child = require('child_process');
-    child.exec(`curl -s "${statement}"`, (error, stdout, stderr) => {
-        currentSong.name = JSON.parse(stdout).recenttracks.track[0].name;
-        currentSong.artist = JSON.parse(stdout).recenttracks.track[0].artist['#text'];
-        kuro.modules['playing'].save(`${currentSong.name} by ${currentSong.artist}`)
-        kuro.user.setGame(`${currentSong.name} by ${currentSong.artist}`)
-    });
+function updatePlaying(_msg) {
+        let statement = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${_config.username}&api_key=${_config.apiKey}&format=json&nowplaying=true`
+        request(statement, function(error, response, body) {
+                if (!error && response.statusCode === 200)
+                {
+                        currentSong.name == JSON.parse(response).recenttracks.track[0].name;
+                        currentSong.artist == KSON.parse(response).recenttracks.track[0].artist['#test'];
+                        if (currentSong.name)
+                        {
+                                if (currentSong.artist)
+                                {
+                                        kuro.modules['playing'].save(`${currentSong.name} by ${currentSong.artist}`)
+                                } else {
+                                        _msg.edit(`Unble to get artist`)
+                                        kuro.log(`Unable to get artist`)
+                                }
+                        } else {
+                                _msg.edit(`Unable to get song name`)
+                                kuro.log(`Unable to get song name`)
+                        }
+                } else {
+                        _msg.edit(`ERROR: Response Code ${response.statusCode}`);
+                        kuro.log(`Last.FM Respone code ${response.statusCode}`)
+                        kuro.log(error)
+                }
+        });
 }
 
 
+
 exports.run = function(msg, args) {
-        updatePlaying()
+        updatePlaying(msg)
         msg.edit(`I am currently listening to "${currentSong.name}" by ${currentSong.artist}`);
 }
