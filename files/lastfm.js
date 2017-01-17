0ea5e119ad1b1e6b7027bfb11a437c121101923d
RyoshiKayo <caymon@ryoshicayo.net>
2017-01-16 21:03:48 -0800

    Create lastfm.js

---
+++ files/lastfm.js
@@ -0,0 +1,10 @@
+module.exports = {
+        //Enable or Disable the Last.FM feature
+        enabled: true,
+        //Your Last.FM username
+        username: '',
+        //Your Last.FM API Key
+        apiKey: '',
+        //Refresh Rate
+        refresh: '15000'
+}
