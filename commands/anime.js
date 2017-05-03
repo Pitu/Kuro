8a922346a4b5a0427c20a0bc68671e6c70f6f7a7
Pitu <seligmannkevin@gmail.com>
2017-05-03 12:26:00 -0300

    Added anime and manga search

---
+++ commands/anime.js
@@ -0,0 +1,62 @@
+const Kitsu = require('kitsu.js')
+const kitsu = new Kitsu()
+let kuro
+
+exports.init = function(bot) { kuro = bot }
+
+exports.run = function(msg, args) {
+	let search = args.toString().replace(/,/g, ' ')
+	kitsu.searchAnime(search)
+		.then(result => {
+			if (result.length === 0) {
+				return msg.edit(`No results found for: **${search}**`)
+			}
+			return prepareEmbed(msg, result[0])
+		})
+		.catch(err => {
+			console.error(err)
+			return msg.edit('There was an error processing the search, please check the console')
+		});
+}
+
+function prepareEmbed(msg, item) {
+	console.log(item)
+	const { slug, synopsis, titles, averageRating, posterImage, episodeLength, showType } = item
+	const url = `https://kitsu.io/anime/${slug}`
+
+	msg.edit('', {
+		embed: {
+			type: 'rich',
+			title: titles.romaji,
+			url,
+			description: `**Synopsis:**\n${synopsis.substring(0, 450)}...`,
+			color: kuro.config.embedColor,
+			fields: [
+				{
+					name: '❯ Type',
+					value: fixCase(showType),
+					inline: true
+				},
+				{
+					name: '❯ Episodes',
+					value: episodeLength,
+					inline: true
+				},
+				{
+					name: '❯ Rating',
+					value: averageRating,
+					inline: true
+				}
+			],
+			author: {
+				name: 'kitsu.io',
+				url: 'https://kitsu.io'
+			},
+			thumbnail: { url: posterImage.small }
+		}
+	})
+}
+
+function fixCase(str) {
+	return str.toLowerCase().replace(/(^| )(\w)/g, s => s.toUpperCase())
+}
---
+++ commands/manga.js
@@ -0,0 +1,67 @@
+const Kitsu = require('kitsu.js')
+const kitsu = new Kitsu()
+let kuro
+
+exports.init = function(bot) { kuro = bot }
+
+exports.run = function(msg, args) {
+	let search = args.toString().replace(/,/g, ' ')
+	kitsu.searchManga(search)
+		.then(result => {
+			if (result.length === 0) {
+				return msg.edit(`No results found for: **${search}**`)
+			}
+			return prepareEmbed(msg, result[0])
+		})
+		.catch(err => {
+			console.error(err)
+			return msg.edit('There was an error processing the search, please check the console')
+		});
+}
+
+function prepareEmbed(msg, item) {
+	console.log(item)
+	const { slug, synopsis, titles, popularityRank, posterImage, chapterCount, volumeCount, mangaType } = item
+	const url = `https://kitsu.io/manga/${slug}`
+
+	msg.edit('', {
+		embed: {
+			type: 'rich',
+			title: titles.enJp,
+			url,
+			description: `**Synopsis:**\n${synopsis.substring(0, 450)}...`,
+			color: kuro.config.embedColor,
+			fields: [
+				{
+					name: '❯ Type',
+					value: fixCase(mangaType),
+					inline: true
+				},
+				{
+					name: '❯ Rank',
+					value: popularityRank,
+					inline: true
+				},
+				{
+					name: '❯ Volumes',
+					value: volumeCount || '-',
+					inline: true
+				},
+				{
+					name: '❯ Chapters',
+					value: chapterCount || '-',
+					inline: true
+				}
+			],
+			author: {
+				name: 'kitsu.io',
+				url: 'https://kitsu.io'
+			},
+			thumbnail: { url: posterImage.small }
+		}
+	})
+}
+
+function fixCase(str) {
+	return str.toLowerCase().replace(/(^| )(\w)/g, s => s.toUpperCase())
+}
